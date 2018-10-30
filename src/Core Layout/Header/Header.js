4ec220bfee73b9ab117cb81bd2098017fa093711
Diksha-Singh <40977095+Diksha-Singh@users.noreply.github.com>
2018-10-30 20:25:31 +0530

    Core Layout

---
+++ src/Core Layout/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+export default class Header extends React.Component {
+    render() {
+        return(
+            <div className="main-header">
+                REACT STUDIO
+            </div>
+        );
+    }
+}
