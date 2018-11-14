1846fdb96dccbf123b5d4836268c7631c287e94c
Diksha-Singh <40977095+Diksha-Singh@users.noreply.github.com>
2018-11-14 14:20:34 +0530

    Export button

--- src/Core Layout/Header/Header.js
+++ src/Core Layout/Header/Header.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './header.css';
+import { Parseit } from './HTMLParser';
 
 export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    downloadContent = (name, content) => {
+        var atag = document.createElement("a");
+        var file = new Blob([content], { type: 'text/plain' });
+        atag.href = URL.createObjectURL(file);
+        atag.download = name;
+        atag.click();
+    }
+
+    handleClick = () => {
+        var components = Parseit();
+        var arr = Object.keys(components);
+        console.log(`array`, arr);
+        for (var i = 0; i < arr.length; i++) {
+            this.downloadContent(`${arr[i]}.js`, components[`${arr[i]}`]);
+        }
+    }
     render() {
-        return(
+
+        return (
             <div className="main-header">
                 REACT STUDIO
+                <button onClick={this.handleClick}>EXPORT</button>
             </div>
         );
     }
